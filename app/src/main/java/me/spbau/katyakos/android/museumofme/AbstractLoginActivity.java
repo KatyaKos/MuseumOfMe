fd8a5988df613dfb6c6cb9a9db0ce6bf289616cf
Catherine Koshchenko <catherine.pths@gmail.com>
2016-12-18 16:51:08 +0300

    Added AbstractLoginActivity.
    Extended LoginActivity and SignupActivity from AbstractLoginActivity.

---
+++ app/src/main/java/me/spbau/katyakos/android/museumofme/AbstractLoginActivity.java
@@ -0,0 +1,120 @@
+package me.spbau.katyakos.android.museumofme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.util.Patterns.EMAIL_ADDRESS;
+
+abstract class AbstractLoginActivity extends AppCompatActivity {
+    protected EditText emailText;
+    protected EditText passwordText;
+    protected TextView link;
+    protected Button button;
+
+    protected int activityId;
+    protected String onVerificationFailMessage;
+
+    protected static final ArrayList<Pair<String, String>> DUMMY_CREDENTIALS = new ArrayList<>(Arrays.asList(
+            new Pair<>("foo@example.com", "hello"), new Pair<>("bar@example.com", "world")
+    ));
+
+    public AbstractLoginActivity() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activityId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verification();
+            }
+        });
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linkClick();
+            }
+        });
+    }
+
+    abstract void linkClick();
+
+    protected void verification() {
+        if (verificationUIStart() == 0) {
+            return;
+        }
+        verificationLogic();
+        verificationUIFinish();
+    }
+
+    protected int verificationUIStart() {
+        int validateResult = validate();
+        if (validateResult > 0) {
+            Toast.makeText(getBaseContext(), onVerificationFailMessage, Toast.LENGTH_LONG).show();
+            button.setEnabled(true);
+            onVerificationFail(validateResult);
+            return 0;
+        }
+
+        button.setEnabled(false);
+        return 1;
+    }
+
+    protected void verificationUIFinish() {
+        button.setEnabled(true);
+    }
+
+    protected void verificationLogic() {
+        if (checkFields()) {
+            onVerificationSuccess();
+        } else {
+            onCheckFail();
+        }
+    }
+
+    abstract void onVerificationSuccess();
+
+    protected void onVerificationFail(int result) {
+        if (result == 1) {
+            emailText.setError("enter a valid email address");
+            emailText.requestFocus();
+        } else if (result == 2) {
+            passwordText.setError("between 4 and 10 alphanumeric characters");
+            passwordText.requestFocus();
+        }
+    }
+
+    abstract boolean checkFields();
+
+    abstract void onCheckFail();
+
+    protected int validate() {
+        int isValid = 0;
+        String email = getStringTextView(emailText);
+        String password = getStringTextView(passwordText);
+
+        if (email.isEmpty() || !EMAIL_ADDRESS.matcher(email).matches()) {
+            isValid = 1;
+        } else if (password.length() < 4 || password.length() > 10) {
+            isValid = 2;
+        }
+        return isValid;
+    }
+
+    protected String getStringTextView(TextView textView) {
+        return textView.getText().toString();
+    }
+}
--- app/src/main/java/me/spbau/katyakos/android/museumofme/SignupActivity.java
+++ app/src/main/java/me/spbau/katyakos/android/museumofme/SignupActivity.java
@@ -1,131 +1,79 @@
 package me.spbau.katyakos.android.museumofme;
 
-import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Pair;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-/**
- * Created by KatyaKos on 14.11.2016.
- */
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
-public class SignupActivity extends Activity {
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
+public class SignupActivity extends AbstractLoginActivity {
+    @InjectView(R.id.signup_input_name)
+    EditText nameText;
 
-    private EditText nameText;
-    private EditText emailText;
-    private EditText passwordText;
-    private Button signupButton;
-    private TextView loginLink;
+    public SignupActivity() {
+        emailText = (EditText) findViewById(R.id.signup_input_email);
+        passwordText = (EditText) findViewById(R.id.signup_input_password);
+        link = (TextView) findViewById(R.id.signup_link_login);
+        button = (Button) findViewById(R.id.signup_button);
+        activityId = R.layout.activity_signup;
+        onVerificationFailMessage = "Signing up failed";
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
-
-        nameText = (EditText) findViewById(R.id.signup_input_name);
-        emailText = (EditText) findViewById(R.id.signup_input_email);
-        passwordText = (EditText) findViewById(R.id.signup_input_password);
-        signupButton = (Button) findViewById(R.id.signup_button);
-        loginLink = (TextView) findViewById(R.id.signup_link_login);
-
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signup();
-            }
-        });
-
-        loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                finish();
-            }
-        });
+        ButterKnife.inject(this);
     }
 
-    public void signup() {
-
-        if (!validate()) {
-            Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-            signupButton.setEnabled(true);
-            return;
-        }
-
-        String name = nameText.getText().toString();
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-
-        if (!checkEmail(email)) {
-            Toast.makeText(getBaseContext(), "This email already has account", Toast.LENGTH_LONG).show();
-            signupButton.setEnabled(true);
-            return;
-        }
-
-        signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        // TODO: Implement your own signup logic here.
-
-        progressDialog.cancel();
-        signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
+    @Override
+    void linkClick() {
+        setResult(0);
         finish();
     }
 
-    private boolean checkEmail(String email) {
-        for (String credential : DUMMY_CREDENTIALS) {
-            String[] pieces = credential.split(":");
-            if (pieces[0].equals(email)) {
+    @Override
+    void onVerificationSuccess() {
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    @Override
+    boolean checkFields() {
+        String email = getStringTextView(emailText);
+        for (Pair<String, String> credential : DUMMY_CREDENTIALS) {
+            String emailCredential = credential.first;
+            if (emailCredential.equals(email)) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean validate() {
-        boolean valid = true;
+    @Override
+    void onCheckFail() {
+        Toast.makeText(getBaseContext(), "This email already has account", Toast.LENGTH_LONG).show();
+    }
 
-        String name = nameText.getText().toString();
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-
-        if (name.isEmpty() || name.length() < 3) {
+    @Override
+    protected void onVerificationFail(int result) {
+        super.onVerificationFail(result);
+        if (result == 3) {
             nameText.setError("at least 3 characters");
-            valid = false;
-        } else {
-            nameText.setError(null);
+            nameText.requestFocus();
         }
+    }
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            emailText.setError(null);
+    @Override
+    protected int validate() {
+        int isValid = super.validate();
+        String name = getStringTextView(nameText);
+        if (name.length() < 3) {
+            isValid = 3;
         }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            passwordText.setError(null);
-        }
-
-        return valid;
+        return isValid;
     }
 }
